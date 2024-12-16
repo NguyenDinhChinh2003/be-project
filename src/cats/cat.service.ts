1c621923fae885c1b9258f3ae75badafb733b73b
ChinhsNguyenex <chinh2584@gmail.com>
2024-12-16 17:49:00 +0700

    setup-service

--- src/cats/cat.service.ts
+++ src/cats/cat.service.ts
@@ -3,7 +3,5 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from "typeorm";
 @Injectable() 
 export class catSerivce{
-    constructor(
-        @InjectRepository(cat) private propertyRepo: Repository<cat>,
-    ){};
+
 }
