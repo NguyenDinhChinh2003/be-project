778d21bf544abd5b1afb1a51551da8419e2f19f6
chinh-agiletech <chinh.nd@agiletechsoftware.com>
2025-02-04 14:47:20 +0700

    update-service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,24 +3,23 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from 'src/entities/user.entity'
+import { User } from 'src/entities/user.entity';
 
 @Injectable()
 export class UserService {
-
-  constructor(@InjectRepository(User) private UserRepo: Repository<User>){}
+  constructor(@InjectRepository(User) private UserRepo: Repository<User>) {}
 
   async create(createUserDto: CreateUserDto) {
     const user = await this.UserRepo.create(createUserDto);
     return await this.UserRepo.save(user);
   }
 
-  async findEmail(email: string){
+  async findEmail(email: string) {
     return await this.UserRepo.findOne({
       where: {
-        email
-      }
-    })
+        email,
+      },
+    });
   }
 
   findAll() {
@@ -29,11 +28,19 @@ export class UserService {
 
   findOne(id: number) {
     return this.UserRepo.findOne({
-      where:{
-        id
+      where: {
+        id,
       },
-      select: ['firstName','lastName','avatarUrl']
-    })
+      select: ['firstName', 'lastName', 'avatarUrl'],
+    });
+  }
+
+  findOneByName(firstName: string) {
+    return this.UserRepo.findOne({
+      where: {
+        firstName,
+      },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
